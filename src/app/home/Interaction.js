21e517e9306c3ed386cc862c40b00bbbaf24f62f
iara benz <iara.benz@hotmail.com>
2023-02-21 18:35:09 +0100

    a

---
+++ src/app/home/Interaction.js
@@ -0,0 +1,51 @@
+/* document.addEventListener("mousemove", (e) => {
+
+    console.log(e)
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const anchor = document.getElementById("anchor")
+    const rekt = anchor.getBoundingClientRect();
+    const anchorX = rekt.left + rekt.width / 2;
+    const anchorY = rekt.top + rekt.height / 2;
+
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+
+    console.log(angleDeg)
+
+    const eyes = document.querySelectorAll(".eye")
+    eyes.forEach(eye => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+
+    })
+}
+)
+
+function angle (cx, cy, ex, ey) {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = rad * 180 / Math.PI;
+    return deg;
+} */
+/* const container = document.querySelector('.container');
+container.addEventListener('mousemove', e => {
+  const eyes = document.querySelectorAll('.eye');
+  [].forEach.call(eyes, function(eye) {
+    // Get the mouse position on the horizontal axis
+    let mouseX = eye.getBoundingClientRect().right;
+    // If it's the left eye we need the left offset instead the right
+    if (eye.classList.contains('eye-left')) {
+      mouseX = eye.getBoundingClientRect().left;
+    }
+    // Now we also need the vertical offset
+    let mouseY = eye.getBoundingClientRect().top;
+    // Now we are going to calculate the radian value of the offset between the mouse and the eye
+    let radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
+    // Let's convert this into a degree based value so we can use it in CSS
+    let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
+    // Now all we have to do is add this degrees to our eye!
+    eye.style.transform = `rotate(${rotationDegrees}deg)`;
+  });
+}); */
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+@HostListener('document:mousemove', ['$event'])
+
+anchorX = 0;
+anchorY = 0;
+anchor: HTMLElement | null = null;
+rekt: DOMRect;
+
+ngOnInIt(): void {
+  if(this.anchor !== null) {
+    if(this.rekt !== null) {
+      this.rekt = this.anchor.getBoundingClientRect();
+      
+      this.anchorX = this.rekt.left + this.rekt.width / 2;
+      this.anchorY = this.rekt.top + this.rekt.height / 2;
+      
+      this.anchor = document.getElementById("anchor");
+    }
+  }
+
+  document.addEventListener("mousemove", (e) => {
+
+  }
+  );
+
 }
+
+onMouseMove(e: { clientX: any; clientY: any; }){
+  
+    console.log(e)
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+
+    const angleDeg = this.angle(mouseX, mouseY, this.anchorX, this.anchorY);
+
+    console.log(angleDeg);
+
+    const eyes = document.querySelectorAll(".eye");
+    console.dir(eyes);
+    eyes.forEach(eye => { 
+        /* eye.style.transform = `rotate(${90 + angleDeg}deg)`; */
+
+        console.dir(eye);
+    })
+}
+
+angle (cx: number, cy: number, ex: number, ey: number) {
+  const dy = ey - cy;
+  const dx = ex - cx;
+  const rad = Math.atan2(dy, dx);
+  const deg = rad * 180 / Math.PI;
+  return deg;
+}
+}
+
